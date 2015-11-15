f39af17bb0e94d6112d43945a32e1349e9cb66c7
Matthew Bernhardt <mjbernha@mit.edu>
2015-11-15 00:29:07 -0500

    Adds detail

--- mrd-dataviz.php
+++ mrd-dataviz.php
@@ -33,12 +33,57 @@
  * Custom post type function
  */
 function custom_posttypes() {
+	$labels = array(
+		'name' => 'Visualizations',
+		'singular_name' => 'Visualization',
+		'menu_name' => 'Visualizations',
+		'name_admin_bar' => 'Visualization',
+		'add_new' => 'Add New',
+		'add_new_item' => 'Add New Visualization',
+		'new_item' => 'New Visualization',
+		'edit_item' => 'Edit Visualization',
+		'view_item' => 'View Visualization',
+		'all_items' => 'All Visualizations',
+		'search_items' => 'Search Visualizations',
+		'parent_item_colon' => 'Parent Visualizations:',
+		'not_found' => 'No visualizations found.',
+		'not_found_in_trash' => 'No visualizations found in Trash.',
+	);
 	$args = array(
+		'labels' => $labels,
 		'public' => true,
-		'label' => 'Data Visualizations',
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'menu_icon' => 'dashicons-chart-area',
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'visualizations' ),
+		'capability_type' => 'post',
+		'has_archive' => true,
+		'hierarchical' => false,
+		'supports' => array( 'title', 'editor', 'thumbnail' ),
 	);
 	register_post_type( 'dataviz', $args );
 
 }
 
 add_action( 'init', 'custom_posttypes' );
+
+/**
+ * Flush rewrites on activation
+ */
+function custom_posttypes_flush() {
+	// First, we "add" the custom post type via the above function (custom_posttypes).
+	// Note: "add" is written with quotes, as custom post types don't get added to the database,
+	// They are only referenced in the post_type column with a post entry when you
+	// add a post of this type.
+	// (Don't get me started on Wordpress' data model)
+	custom_posttypes();
+
+	// ATTENTION: This is *only* done during plugin activation.
+	// You should never do this on every page load.
+	flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'custom_posttypes_flush' );
