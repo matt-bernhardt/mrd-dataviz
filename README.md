816fa33f0aefd946e67789c43ff1f33665068b09
Matthew Bernhardt <mjbernha@mit.edu>
2015-11-14 23:34:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mrd-dataviz
+
+This Wordpress plugin provides a "Data Viz" post type, which allows authors to create data visualization posts with embedded javascript and data files.
---
+++ mrd-dataviz.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: MRD DataViz
+ * Plugin URI: https://github.com/matt-bernhardt/mrd-dataviz
+ * Description: A post type plugin for data visualizations
+ * Version: 0.1
+ * Author: Matt Bernhardt
+ * Author URI: https://github.com/matt-bernhardt
+ * License: GPL2
+ *
+ * @package MRD DataViz
+ * @author Matt Bernhardt
+ * @link https://github.com/matt-bernhardt/mrd-dataviz
+ */
+
+/**
+ * MRD DataViz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * MRD DataViz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MRD DataViz. If not, see 
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html.
+ */
+?>
